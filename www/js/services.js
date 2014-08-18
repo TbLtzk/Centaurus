1bf08390f53c180e9a73e4874b1700e2a70917b8
klopper <2014@powderfan.de>
2014-08-18 22:38:49 +0200

    auto refresh on each ledger

--- www/js/services.js
+++ www/js/services.js
@@ -3,29 +3,35 @@ angular.module('starter.services', [])
 .factory('Account', function($rootScope, Settings, Remote) {
   
   var keys = Settings.getKeys();
+  var account = { address: keys.address, balance: 0, reserve:20, devInfo: keys };
+  
   var remote = Remote.get();
   
-  
-  var account = { address: keys.address, balance: 0, reserve:50, devInfo: keys };
-  
-  remote.connect(function() {
-  console.log('remote connected')
-
-  var hasFinished = false;
-  request = remote.requestAccountInfo(keys.address, function(err, res) {
-		if (err){}
+  var updateAccountInfo = function() {
+	  var request = remote.requestAccountInfo(keys.address, function(err, res) {
+		if (err){
+			console.error('Could not retrieve account info:')
+			console.error(res.remote.error_message)
+			console.error(res)
+		}
 		else {
-			console.log('You have ' + res.account_data.Balance/1000000 + ' (STR)');	
+			console.log('accountInfoLoaded');	
 			account.balance = res.account_data.Balance/1000000;
 			$rootScope.$broadcast('accountInfoLoaded');
 		}		
-	});  
-  request.request();  
+	  })
+      request.request();
+  };
+  
+  updateAccountInfo(); // initial account info
+  
+  remote.on('ledger_closed', function(data){
+    updateAccountInfo(); // update account info
   });
-
+  
   return {
-    get: function() {
-      return account;
+    get: function(){
+	  return account;
     }
   }
 })
@@ -41,11 +47,18 @@ var remote = new Remote({
   fee_cushion:     1.5,
   servers: [
     {
-        host:    'live.stellar.org'
+        host:    'test.stellar.org'
       , port:    9001
       , secure:  true
     }
   ]
+});
+
+var keys = Settings.getKeys();
+remote.set_secret(keys.address, keys.secret);
+
+remote.connect(function() {
+  console.log('remote connected')
 });
 
 return {
@@ -57,7 +70,7 @@ return {
 
 .factory('Settings', function() {
   
-  var defaultKeys = { address: 'gEux6NhrybVLuvgaYrgThTk4d3Kmd3s4NP', secret:'mySecret' };
+  var defaultKeys = { address: 'gEux6NhrybVLuvgaYrgThTk4d3Kmd3s4NP', secret:'mySecret' }; // live network
   
   var keysString = window.localStorage['keys'];
   var keys;
@@ -70,6 +83,9 @@ return {
     keys = JSON.parse(keysString);
 	keys.mode = 'loaded';
   }  
+
+  // override for use in test network
+  var keys = { address: 'gHBsnApP6wutZweigvyADvxHmwKZVkAFwY', secret:'s3qgYLVJQJL2eLZ54TB9msjmpRHXQBdQrmG9WbD6yVCx6NrFMYU', mode: 'test'}; 
 
   return {
     getKeys: function() {
