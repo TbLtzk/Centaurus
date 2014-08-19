2420a6bb8104f6070024ea14a904691f08bf2885
chrosim <chrosim@gmail.com>
2014-08-19 18:30:55 +0200

    added loading for account creation

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,53 +1,62 @@
 angular.module('starter.controllers', [])
-.controller('WalletCtrl', function($scope, Account) {  
-  $scope.$on('accountInfoLoaded', function(event){  
-	$scope.account = Account.get();		
-	$scope.$apply();
-  });
-  $scope.account = Account.get();	
-})
-
-.controller('SendCtrl', function($scope, Account, Remote) {
-  var account = Account.get();
-
-  $scope.paymentData = { destinationAddress: 'gEPLboQjouwdRBoVzi8vwLd2SWjZa3xcTL', amount: '1', currency:'STR' }   
-  
-  $scope.available = account.balance - account.reserve;
-  $scope.account = account;
-  
-  $scope.sendPayment = function(){
-	var Amount = stellar.Amount;
-	
-	var remote = Remote.get();
-	var transaction = remote.transaction();
-	var amountApi = Amount.from_human($scope.paymentData.amount + $scope.paymentData.currency);
-	
-	transaction.payment({ from: account.address, to: $scope.paymentData.destinationAddress, amount: amountApi });
-	
-	transaction.submit(function(err, res){
-	  // TODO: some visible feedback (and block UI until finished)
-      if(err){
-        console.error('Payment ' + JSON.stringify( $scope.paymentData ) + ' failed:');
-        console.error(err);
-      }
-      else {
-        console.log('Payment ' + JSON.stringify( $scope.paymentData ) + ' succesful!');
-      }	  
+.controller('WalletCtrl', function ($scope, Account) {
+	$scope.$on('accountInfoLoaded', function (event) {
+		$scope.account = Account.get();
+		$scope.$apply();
 	});
-  };
+	$scope.account = Account.get();
 })
 
-.controller('ReceiveCtrl', function($scope, Account) {
-  $scope.account = Account.get();
-new QRCode(document.getElementById("QRCODE")
-   , {width:128, height: 128, text:Account.get().address});
+.controller('SendCtrl', function ($scope, Account, Remote) {
+	var account = Account.get();
+
+	$scope.paymentData = {
+		destinationAddress : 'gEPLboQjouwdRBoVzi8vwLd2SWjZa3xcTL',
+		amount : '1',
+		currency : 'STR'
+	}
+
+	$scope.available = account.balance - account.reserve;
+	$scope.account = account;
+
+	$scope.sendPayment = function () {
+		var Amount = stellar.Amount;
+
+		var remote = Remote.get();
+		var transaction = remote.transaction();
+		var amountApi = Amount.from_human($scope.paymentData.amount + $scope.paymentData.currency);
+
+		transaction.payment({
+			from : account.address,
+			to : $scope.paymentData.destinationAddress,
+			amount : amountApi
+		});
+
+		transaction.submit(function (err, res) {
+			// TODO: some visible feedback (and block UI until finished)
+			if (err) {
+				console.error('Payment ' + JSON.stringify($scope.paymentData) + ' failed:');
+				console.error(err);
+			} else {
+				console.log('Payment ' + JSON.stringify($scope.paymentData) + ' succesful!');
+			}
+		});
+	};
+})
+
+.controller('ReceiveCtrl', function ($scope, Account) {
+	$scope.account = Account.get();
+	new QRCode(document.getElementById("QRCODE"), {
+		width : 128,
+		height : 128,
+		text : Account.get().address
+	});
 
 })
 
-.controller('AboutCtrl', function($scope) {
+.controller('AboutCtrl', function ($scope) {
 
-  $scope.GotoLink = function (url) {
-    window.open(url,'_system');
-  }
-})
-;
+	$scope.GotoLink = function (url) {
+		window.open(url, '_system');
+	}
+});
