bc0c3486ebf4611e51825075bb4d51752ade9912
Tobias Latzke <centaurus@xcoins.de>
2015-10-13 23:18:55 +0200

    wrapped with a VS TACO project

--- www/js/app.js
+++ www/js/app.js
@@ -9,7 +9,7 @@ angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter
 	$ionicPlatform.ready(function () {
 		// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
 		// for form inputs)
-		if (window.cordova && window.cordova.plugins.Keyboard) {
+	    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
 			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 		}
 		if (window.StatusBar) {
