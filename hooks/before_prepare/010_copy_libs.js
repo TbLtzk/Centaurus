ee85533f15bba3d9c7019cbcbec6166e05978f29
Martijn van Eijk <martijn@quantoz.com>
2016-02-17 16:10:48 +0100

    Fixed ngCordova

--- hooks/before_prepare/010_copy_libs.js
+++ hooks/before_prepare/010_copy_libs.js
@@ -14,7 +14,7 @@ var libs = [
     'ionic/release/css/ionic.min.css'
   , 'ionic/release/fonts'
   , 'ionic/release/js/ionic.bundle.min.js'
-  , 'ng-cordova/dist/ng-cordova.min.js'
+  , 'ngCordova/dist/ng-cordova.min.js'
   , 'angular-translate/angular-translate.min.js'
   , 'angular-translate-loader-static-files/angular-translate-loader-static-files.min.js'
   , 'stellar-sdk/stellar-sdk.min.js'
@@ -30,7 +30,7 @@ libs.forEach(function(file){
             fs.copySync(sourceDir + file, targetDir + file);
             console.log('010_copy_libs copied ' + file);
         }
-        else 
+        else
             skipped++;
     } catch(err) {
         console.error('010_copy_libs failed to copy ' + file + ' ' + err);
