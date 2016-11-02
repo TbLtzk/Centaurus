35e67003d9b8db558c23c55920656173e545273e
Tbltzk <centaurus@xcoins.de>
2016-11-02 21:03:39 +0100

    switch to normal payment after account creation

--- www/js/controllers/controllers.send.js
+++ www/js/controllers/controllers.send.js
@@ -305,6 +305,7 @@
             .then(function (transactionResult) {
                 console.log(transactionResult);
                 $scope.paymentData.amount = 0;
+                $scope.destinationInfo.needFunding = false;
                 UIHelper.blockScreen('controllers.send.success', 2);
             })
             .catch(function (err) {
